42c61d24e75beda3018d310deb37e1acd280f01c
nishantsh001 <nishant.sh1999@gmail.com>
2024-12-09 16:09:28 +0530

    first commit

---
+++ src/test/java/com/Audio_Transcribe/AudioTranscribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Audio_Transcribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTranscribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
